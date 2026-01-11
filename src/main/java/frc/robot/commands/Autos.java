6db33465f4b543c8a3d6cb452f24e955de7d6a35
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2026-01-11 13:05:45 -0700

    port to 2026

---
+++ src/main/java/frc/robot/commands/Autos.java
@@ -0,0 +1,23 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import frc.robot.RobotContainer;
+import frc.robot.subsystems.commands.DriveToPoint;
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
+
+public final class Autos {
+  /** Example static factory for an autonomous command. */
+  public static Command mainAuto(RobotContainer bot) {
+    return Commands.sequence(new DriveToPoint(bot.m_drive, new Pose2d(0, -1, new Rotation2d(0)), 0.3));
+  }
+
+  private Autos() {
+    throw new UnsupportedOperationException("This is a utility class!");
+  }
+}
