07b2c819ec802e37310e8ecf07c409821813bcd2
SirHoog <121210119+SirHoog@users.noreply.github.com>
2026-01-25 12:51:41 -0700

    Update Shooter.java
    Not done yet. I'm on phone so I'm just switching files rn. Just reused old elevator code from last season

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -10,10 +10,94 @@ import frc.robot.Constants.ShooterConstants;
 public class Shooter extends SubsystemBase {
 
     SparkMax m_motor = new SparkMax(ShooterConstants.kShooterMotorID, MotorType.kBrushed);
+    private SparkMaxConfig m_config = new SparkMaxConfig();
 
-    public Shooter() {}
+    private TrapezoidProfile m_profile = new TrapezoidProfile(new Constraints(???, ???));
+    private TrapezoidProfile.State m_goal = new State();
+    private TrapezoidProfile.State m_setpoint = new State();
+    
+    private PIDController pidConstants = new PIDController(???, 0.0, 0.0);
 
-    public void setPower(double pow) {
-        m_motor.set(pow);
+    private double ff = 0.77; // Feedforward
+
+    private double motorPower = 0;
+
+    public Shooter() {
+        m_config.closedLoop.pid(???, 0, 0);
+        m_config.inverted(true);
+        m_config.idleMode(IdleMode.kBrake);
+        m_config.smartCurrentLimit(???);
+
+        m_elevMotor.configure(m_config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+        SmartDashboard.putData("Shooter PID", pidConstants);
+        
+        SmartDashboard.putNumber("Shooter Feedforward", ff);
+    }
+
+    public void shoot() {
+        // Set the motorPower
+        // and release the ball into the flywheels
+    }
+
+    public void setReference(double val, ControlType type, double ff) {
+        m_elevMotor.getClosedLoopController().setReference(val, type, ClosedLoopSlot.kSlot0, ff);
+    }
+
+    @Override
+    public void periodic() {
+        ff = SmartDashboard.getNumber("Elevator Feedforward", 0.7);
+
+        m_setpoint = m_profile.calculate(0.02, m_setpoint, m_goal);
+
+        setReference(m_setpoint.position, ControlType.kPosition, ff);
+        
+        SmartDashboard.putNumber("Target Elevator Position", m_setpoint.position);
+        SmartDashboard.putNumber("Actual Elevator Position", m_elevMotor.getEncoder().getPosition());
+        SmartDashboard.putNumber("NEO Power", m_elevMotor.get());
+
+        AutoConstants.elevatorHeights = SmartDashboard.getNumberArray("Elevator Heights", new double[] {0, 0, 0, 0});
+        
+        m_config.closedLoop.pid(pidConstants.getP(), pidConstants.getI(), pidConstants.getD());
+        m_elevMotor.configure(m_config, ResetMode.kNoResetSafeParameters, PersistMode.kNoPersistParameters);
+    }
+
+    private void setState(int pos) {
+        m_goal = new TrapezoidProfile.State(AutoConstants.elevatorHeights[pos], 0);
+    }
+
+    public Command setGoal(int pos) {
+        return runOnce(() -> {
+            elevHeight = pos;
+            setState(pos);
+        });
+    }
+
+    public Command down() {
+        return runOnce(() -> {
+            elevHeight--;
+            if(elevHeight < 0) elevHeight = 0;
+            setState(elevHeight);
+        });
+    }
+
+    public Command up() {
+        return runOnce(() -> {
+            elevHeight++;
+            if(elevHeight > 3) elevHeight = 3;
+            setState(elevHeight);
+        });
+    }
+
+    public Command setPos(double pos) {
+        return runOnce(() -> {
+            m_goal = new TrapezoidProfile.State(pos, 0);
+        });
+    }
+
+    public void reset() {
+        elevHeight = 0;
+        
+        m_goal = new TrapezoidProfile.State(0, 0);
+        m_setpoint = new TrapezoidProfile.State(0, 0);
     }
 }
