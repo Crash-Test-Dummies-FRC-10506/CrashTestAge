3d88501ce9438e6bd85bf8a7fcdf8a899a11820e
SirHoog <121210119+SirHoog@users.noreply.github.com>
2026-01-25 14:02:36 -0700

    Update Robot.java

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -104,7 +104,46 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    // Calculate drivetrain commands from Joystick values
+    double forward = -controller.getLeftY()  * 0.668;
+    double strafe  = -controller.getLeftX()  * 0.668;
+    double turn    = -controller.getRightX() * 0.668;
+
+    // Read in relevant data from the Camera
+    boolean targetVisible = false;
+    double targetYaw = 0.0;
+    var results = camera.getAllUnreadResults();
+    if (!results.isEmpty()) {
+      // Camera processed a new frame since last
+      // Get the last one in the list.
+      var result = results.get(results.size() - 1);
+      if (result.hasTargets()) {
+        // At least one AprilTag was seen by the camera
+        for (var target : result.getTargets()) {
+          if (target.getFiducialId() == 7) {
+            // Found Tag 7, record its information
+            targetYaw = target.getYaw();
+            targetVisible = true;
+          }
+        }
+      }
+    }
+
+    // Auto-align when requested
+    if (controller.getAButton() && targetVisible) {
+      // Driver wants auto-alignment to tag 7
+      // And, tag 7 is in sight, so we can turn toward it.
+      // Override the driver's turn command with an automatic one that turns toward the tag.
+      turn = -1.0 * targetYaw * VISION_TURN_kP * 0.668;
+    }
+
+    // Command drivetrain motors based on target speeds
+    drivetrain.drive(forward, strafe, turn);
+
+    // Put debug information to the dashboard
+    SmartDashboard.putBoolean("Vision Target Visible", targetVisible);
+  }
 
   @Override
   public void testInit() {
