3851480728cca6af6ce0f94a8674b9fe264d0a89
NotTacos <128665709+NotTacos2@users.noreply.github.com>
2026-08-09 09:55:39 -0700

    Delete src directory

--- src/main/java/framework/src/main/java/org/frc10506/framework/util/log/transport/Transport.java
+++
@@ -1,9 +0,0 @@
-package framework.src.main.java.org.frc10506.framework.util.log.transport;
-
-import framework.src.main.java.org.frc10506.framework.util.log.Log;
-
-@FunctionalInterface
-public interface Transport {
-
-	void accept(Log log);
-}
--- src/main/java/robot/src/main/java/org/frc10506/rebuilt2026/util/Tuning.java
+++
@@ -1,7 +0,0 @@
-package robot.src.main.java.org.frc10506.viridian.util;
-
-public final class Tuning {
-
-    private Tuning() {
-    }
-}
